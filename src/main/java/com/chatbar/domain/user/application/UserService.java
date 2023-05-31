31b9c472af62bd8d02a799fae14e5b2ef9189b9f
choeun7 <short0720@gmail.com>
2023-05-31 18:07:54 +0900

    [FEAT]: 유저조회 API 개발

--- src/main/java/com/chatbar/domain/user/application/UserService.java
+++ src/main/java/com/chatbar/domain/user/application/UserService.java
@@ -1,9 +1,17 @@
 package com.chatbar.domain.user.application;
 
+import com.chatbar.domain.user.domain.User;
 import com.chatbar.domain.user.domain.repository.UserRepository;
+import com.chatbar.domain.user.dto.UserRes;
+import com.chatbar.global.DefaultAssert;
+import com.chatbar.global.config.security.token.UserPrincipal;
+import com.chatbar.global.payload.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +19,27 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    //유저 조회 (조회 기준 - 유저 id)
+    public ResponseEntity<?> findUser(UserPrincipal userPrincipal) {
+
+        Optional<User> user = userRepository.findById(userPrincipal.getId());
+        DefaultAssert.isTrue(user.isPresent(), "유저가 올바르지 않습니다.");
+
+        UserRes userRes = UserRes.builder()
+                .id(user.get().getId())
+                .email(user.get().getEmail())
+                .nickname(user.get().getNickname())
+                .profileImg(user.get().getProfileImg())
+                .categories(user.get().getCategories())
+                .build();
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information(userRes)
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
+    }
+
 }
